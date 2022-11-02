3f1c436508deacf15aaffd7f924ae3686e38b6d2
daltonmcphaden <dmcphade@uwo.ca>
2022-11-02 09:52:25 -0400

    email cronjob setup

--- netlify/functions/test-scheduled-function.js
+++ netlify/functions/test-scheduled-function.js
@@ -1,7 +1,25 @@
 const { schedule } = require('@netlify/functions');
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+const date = Date.now()
+const converted = new Date(date)
+const formatted = converted.toUTCString()
+
+const msg = {
+    to: process.env.SENDGRID_TO, // Change to your recipient
+    from: process.env.SENDGRID_FROM, // Change to your verified sender
+    subject: 'CronJob ' + formatted,
+    html: 'This email was sent',
+  }
 
 const handler = async function(event, context) {
-    console.log("Received event:", event);
+    sgMail.send(msg).then(() => {
+        console.log('Email sent')
+    })
+    .catch((error) => {
+        console.error(error)
+    })
 
     return {
         statusCode: 200,

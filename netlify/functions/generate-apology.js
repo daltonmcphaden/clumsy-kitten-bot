154ce111165caecd770582420624902ebdea8f93
Dalton McPhaden <44533832+daltonmcphaden@users.noreply.github.com>
2024-01-10 21:02:36 -0500

    apply 2 spaces formatting

--- netlify/functions/generate-apology.js
+++ netlify/functions/generate-apology.js
@@ -7,46 +7,46 @@ cohere.init(process.env.COHERE_API_KEY);
 const items = require("../../household_items.json");
 
 exports.handler = async function (event, context) {
-	const response = await cohere.generate({
-		model: "xlarge",
-		prompt:
-			"Given a common household item, pretend to be a kitten apologizing to their owner for breaking something in the house.\nItem: Lamp\nApology: Hi this is Ellie, sorry for breaking the lamp! Have a nice day, Ellie\n--\nItem: TV\nApology: Hey mom this is Ellie, I accidentally broke the TV! See you later, Ellie\n--\nItem: Curtains\nApology: Hey dad it's Ellie, I tried climbing the curtains and it didn't go so well... Oops! Hope you're having a good day, Ellie\n--\nItem:" +
-			items[Math.floor(Math.random() * items.length)] +
-			"\nApology:",
-		max_tokens: 50,
-		temperature: 2,
-		k: 0,
-		p: 0.75,
-		frequency_penalty: 0,
-		presence_penalty: 0,
-		stop_sequences: ["--"],
-		return_likelihoods: "NONE",
-	});
-	const apology = response.body.generations[0].text.split(/\r?\n/)[0];
+  const response = await cohere.generate({
+    model: "xlarge",
+    prompt:
+      "Given a common household item, pretend to be a kitten apologizing to their owner for breaking something in the house.\nItem: Lamp\nApology: Hi this is Ellie, sorry for breaking the lamp! Have a nice day, Ellie\n--\nItem: TV\nApology: Hey mom this is Ellie, I accidentally broke the TV! See you later, Ellie\n--\nItem: Curtains\nApology: Hey dad it's Ellie, I tried climbing the curtains and it didn't go so well... Oops! Hope you're having a good day, Ellie\n--\nItem:" +
+      items[Math.floor(Math.random() * items.length)] +
+      "\nApology:",
+    max_tokens: 50,
+    temperature: 2,
+    k: 0,
+    p: 0.75,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    stop_sequences: ["--"],
+    return_likelihoods: "NONE",
+  });
+  const apology = response.body.generations[0].text.split(/\r?\n/)[0];
 
-	const fullApology = apology + "\nP.S. Gimme my wet food!"
+  const fullApology = apology + "\nP.S. Gimme my wet food!"
 
-	const date = Date.now();
-	const converted = new Date(date);
-	const formatted = converted.toUTCString();
-	const recipients = process.env.SENDGRID_TO.split(" ");
-	const msg = {
-		to: recipients,
-		from: process.env.SENDGRID_FROM,
-		subject: "Oopsie!",
-		html: fullApology + "<br>Sent at: " + formatted,
-	};
-    console.log(msg.html)
-	await sgMail
-		.send(msg)
-		.then(() => {
-			console.log("Email sent");
-		})
-		.catch((error) => {
-			console.log(error);
-		});
+  const date = Date.now();
+  const converted = new Date(date);
+  const formatted = converted.toUTCString();
+  const recipients = process.env.SENDGRID_TO.split(" ");
+  const msg = {
+    to: recipients,
+    from: process.env.SENDGRID_FROM,
+    subject: "Oopsie!",
+    html: fullApology + "<br>Sent at: " + formatted,
+  };
+  console.log(msg.html)
+  await sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 
-	return {
-		statusCode: 200,
-	};
+  return {
+    statusCode: 200,
+  };
 };
